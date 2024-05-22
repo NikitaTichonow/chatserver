40b0e4e7d675df38fe9d8def65f23345c014106d
Nikita <niktos@icloud.com>
2024-05-22 23:53:03 +0300

    djangochat commit

---
+++ djangochat/chat/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile, Room, Message
+
+admin.site.unregister(User)
+admin.site.register(User)
+
+admin.site.register(UserProfile)
+admin.site.register(Room)
+admin.site.register(Message)
---
+++ djangochat/djangochat/asgi.py
@@ -0,0 +1,14 @@
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import ws_urlpatterns
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoChatServer.settings')
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+})
